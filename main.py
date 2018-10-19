dda6b5e35b5a1298d8aa9a833cb941467ed183d0
elldi <elliotdines1@gmail.com>
2018-10-19 15:37:38 +0100

    code tidy up

--- main.py
+++ main.py
@@ -7,38 +7,22 @@ from io import BytesIO
 
 app = Flask(__name__)
 ## camera = cv2.VideoCapture(0)
+camera = PiCamera()
 
 @app.route('/')
 def index():
-	return render_template('index.html')
-
-@app.route('/live')
-def live():
 	return render_template('live.html')
 
-'''
-@app.route('/login', methods=['POST'])
-def login():
-	print(request.form['hashInput'])
-	print("Logging in")
-	return Response('/live', status=302)
-'''
-
 def gen():
-	print("Starting camera")
-	'''
-	while True:
-		_, frame = camera.read()
-		frame = cv2.imencode('.jpg', frame)[1].tobytes()
-	'''
+	##with camera  as camera:
 	my_stream = BytesIO()
-	camera = PiCamera()
 	for image in camera.capture_continuous(my_stream, 'jpeg', use_video_port=True):
 		my_stream.seek(0)
 		yield (b'--frame\r\n'
-               	b'Content-Type: image/jpeg\r\n\r\n' + my_stream.read() + b'\r\n')
+              	b'Content-Type: image/jpeg\r\n\r\n' + my_stream.read() + b'\r\n')
 		my_stream.seek(0)
 		my_stream.truncate()
+	camera.stop_recording()
 
 @app.route('/video_feed')
 def video_feed():
@@ -46,4 +30,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', debug = True, threaded = True)
+	app.run(host = '0.0.0.0', debug = False, threaded = True)
